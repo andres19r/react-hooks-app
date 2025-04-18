9548a90d76b72e37007d0f00959cb9edcf38c576
Andres Rivero <andres19rivero@gmail.com>
2025-04-18 15:33:39 -0400

    add CallbackHook and ShowIncrement components to demonstrate useCallback for performance optimization

---
+++ src/06-memos/ShowIncrement.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const ShowIncrement = React.memo(({ increment }) => {
+  console.log("Generated again");
+
+  return (
+    <button
+      onClick={() => {
+        increment();
+      }}
+      className="btn btn-primary"
+    >
+      Increment
+    </button>
+  );
+});
