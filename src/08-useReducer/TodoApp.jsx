d2e29aa1676c67c06e7f597f429767b4dc84eb9f
Andres Rivero <andres19rivero@gmail.com>
2025-04-22 13:21:56 -0400

    implement handleNewTodo function and update todoReducer to handle adding new todos

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -20,7 +20,11 @@ export const TodoApp = () => {
   const [todos, dispatch] = useReducer(todoReducer, initialState);
 
   const handleNewTodo = (newTodo = {}) => {
-    console.log({ newTodo });
+    const action = {
+      type: "[TODO] Add Todo",
+      payload: newTodo,
+    };
+    dispatch(action);
   };
 
   return (
