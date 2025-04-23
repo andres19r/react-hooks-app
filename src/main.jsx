f84810ee0efad546d28e20f918b89e3c44d4c448
Andres Rivero <andres19rivero@gmail.com>
2025-04-23 01:14:56 -0400

    add react-router as a dependency and update main.jsx to use BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { HooksApp } from "./HooksApp";
+import { MainApp } from "./09-useContext/MainApp";
+import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <HooksApp />
-  </StrictMode>
+  <BrowserRouter>
+    {/* <StrictMode> */}
+    <MainApp />
+    {/* </StrictMode> */}
+  </BrowserRouter>
 );
