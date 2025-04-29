0d75c2085e8ddf22367c4ed4ff97778b9ec8af8a
Andres Rivero <andres19rivero@gmail.com>
2025-04-29 17:41:13 -0400

    fix: update key prop in PokemonCard component to use index for sprite mapping

--- src/03-examples/PokemonCard.jsx
+++ src/03-examples/PokemonCard.jsx
@@ -3,10 +3,12 @@ import { useLayoutEffect, useRef, useState } from "react";
 export const PokemonCard = ({ id, name, sprites = [] }) => {
   const h2Ref = useRef();
   const [boxSize, setBoxSize] = useState({ width: 0, height: 0 });
+
   useLayoutEffect(() => {
     const { height, width } = h2Ref.current.getBoundingClientRect();
     setBoxSize({ height, width });
   }, [name]);
+
   return (
     <section style={{ height: 200, display: "flex", flexDirection: "row" }}>
       <h2 ref={h2Ref} className="text-capitalize">
@@ -14,8 +16,8 @@ export const PokemonCard = ({ id, name, sprites = [] }) => {
       </h2>
       {/* Images */}
       <div>
-        {sprites.map((sprite) => (
-          <img key={sprite} src={sprite} alt={name} />
+        {sprites.map((sprite, index) => (
+          <img key={index} src={sprite} alt={name} />
         ))}
       </div>
 
