a3bd1808e5aafc988dbbaa1e2c3733078e5efe88
Andres Rivero <andres19rivero@gmail.com>
2025-04-29 23:23:36 -0400

    test: add additional tests for TodoItem component functionality

--- tests/08-useReducer/TodoItem.test.jsx
+++ tests/08-useReducer/TodoItem.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { TodoItem } from "../../src/08-useReducer/TodoItem";
 
 describe("<TodoItem /> tests", () => {
@@ -33,5 +33,56 @@ describe("<TodoItem /> tests", () => {
     );
     expect(spanElement.className).toContain("align-self-center");
     expect(spanElement.className).not.toContain("text-decoration-line-through");
+  });
+
+  test("should show the todo completed", () => {
+    render(
+      <TodoItem
+        id={todo.id}
+        description={todo.description}
+        done={true}
+        onToggleTodo={onToggleTodoMock}
+        onDeleteTodo={onDeleteTodoMock}
+      />
+    );
+
+    const spanElement = screen.getByLabelText("span");
+
+    expect(spanElement.className).toContain("text-decoration-line-through");
+  });
+
+  test("should call ToggleTodo by span on click", () => {
+    render(
+      <TodoItem
+        id={todo.id}
+        description={todo.description}
+        done={todo.done}
+        onToggleTodo={onToggleTodoMock}
+        onDeleteTodo={onDeleteTodoMock}
+      />
+    );
+
+    const spanElement = screen.getByLabelText("span");
+    fireEvent.click(spanElement);
+
+    expect(onToggleTodoMock).toHaveBeenCalled();
+    expect(onToggleTodoMock).toHaveBeenCalledWith(todo.id);
+  });
+
+  test("should call DeleteTodo by buttton on click", () => {
+    render(
+      <TodoItem
+        id={todo.id}
+        description={todo.description}
+        done={todo.done}
+        onToggleTodo={onToggleTodoMock}
+        onDeleteTodo={onDeleteTodoMock}
+      />
+    );
+    const deleteBtn = screen.getByRole("button");
+    fireEvent.click(deleteBtn);
+
+    expect(onDeleteTodoMock).toHaveBeenCalled();
+    expect(onDeleteTodoMock).toHaveBeenCalledWith(todo.id);
   });
 });
